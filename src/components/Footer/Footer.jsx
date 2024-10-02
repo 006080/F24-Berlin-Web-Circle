c3bbd539991251b89d51049f0c11b106219829f0
006080 <nataliyarodionova28@gmail.com>
2024-10-02 20:59:55 +0200

    socialMediaIcons

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,9 +1,22 @@
 import "./Footer.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faTwitter,
+  faSquareInstagram,
+} from "@fortawesome/free-brands-svg-icons";
 
 const Footer = () => {
   return (
     <footer>
       <div>© 2024 Airbnb, Inc.</div>
+      <div className="socialMedia">
+      
+        <FontAwesomeIcon style={{marginLeft:"10px"}} icon={faFacebook} />
+        <FontAwesomeIcon style={{marginLeft:"10px"}} icon={faTwitter} />
+          <FontAwesomeIcon  style={{marginLeft:"10px"}} icon={faSquareInstagram} />
+     
+      </div>
     </footer>
   );
 };
