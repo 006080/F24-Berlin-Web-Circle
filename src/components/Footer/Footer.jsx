92a132b0d4bd4d5443e8ac54dcd6e9206ea5cc72
006080 <nataliyarodionova28@gmail.com>
2024-10-03 13:20:22 +0200

    social links

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,11 +11,33 @@ const Footer = () => {
     <footer>
       <div>© 2024 Airbnb, Inc.</div>
       <div className="socialMedia">
-      
-        <FontAwesomeIcon style={{marginLeft:"10px"}} icon={faFacebook} />
-        <FontAwesomeIcon style={{marginLeft:"10px"}} icon={faTwitter} />
-          <FontAwesomeIcon  style={{marginLeft:"10px"}} icon={faSquareInstagram} />
-     
+
+        <a
+          href="https://www.facebook.com/airbnb" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ marginLeft: "10px", color: "black", fontSize: "20px"}}
+        >
+          <FontAwesomeIcon icon={faFacebook} />
+        </a>
+
+        <a
+          href="https://www.twitter.com/airbnb" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ marginLeft: "10px", color: "black", fontSize: "20px" }}
+        >
+          <FontAwesomeIcon icon={faTwitter} />
+        </a>
+
+        <a
+          href="https://www.instagram.com/airbnb" 
+          target="_blank" 
+          rel="noopener noreferrer"
+          style={{ marginLeft: "10px", color: "black",fontSize: "20px" }}
+        >
+          <FontAwesomeIcon icon={faSquareInstagram} />
+        </a>
       </div>
     </footer>
   );
